4195127547cc3a481f575617f20fc037e438c621
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-08-20 23:01:23 +0300

    Second commit

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -24,26 +24,24 @@ namespace ControlWork
                 newCity.Name = nameSplit[0];
                 newCity.Populated = int.Parse(values[0]);
                 newCity.Area = int.Parse(values[1]);
-                
                 newCity.Density = Convert.ToDouble(newCity.Populated) / Convert.ToDouble(newCity.Area);
                 cities[i] = newCity;
             }
 
             string longestName = string.Empty;
             string mostPopulated = string.Empty;
-            int maxpeopple = 0;
+            int maxPeopple = 0;
 
             for (int i = 0; i < cities.Length; i++)
             {
                 if (longestName.Length < cities[i].Name.Length)
                 {
-                    
                     longestName = cities[i].Name;
                 }
-                if (maxpeopple < cities[i].Populated)
+                if (maxPeopple < cities[i].Populated)
                 {
-                    maxpeopple = cities[i].Populated;
-                    mostPopulated = $"{cities[i].Name} ({maxpeopple} peopple)";
+                    maxPeopple = cities[i].Populated;
+                    mostPopulated = $"{cities[i].Name} ({maxPeopple} peopple)";
                 }
             }
 
@@ -54,9 +52,6 @@ namespace ControlWork
             {
                 Console.WriteLine($"\t{c.Name} - {Math.Round(c.Density,2)}");
             }
-
-
-
 
             Console.ReadLine();
         }
