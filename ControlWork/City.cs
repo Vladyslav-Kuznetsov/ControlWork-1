c3e03f59a8bc2e2fb31af19f1aae9f4268ae33e5
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-08-21 01:44:30 +0300

    Fixed

---
+++ ControlWork/City.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ControlWork
+{
+    struct City
+    {
+        public int Populated;
+        public string Name;
+        public int Area;
+        public double Density;
+        public City(string result)
+        {
+            string[] nameSplit = result.Split('=');
+            string[] values = nameSplit[1].Split(',');
+            Name = nameSplit[0];
+            Populated = int.Parse(values[0]);
+            Area = int.Parse(values[1]);
+            Density = Convert.ToDouble(Populated) / Convert.ToDouble(Area);
+        }
+    }
+}
--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -2,38 +2,20 @@
 
 namespace ControlWork
 {
-    struct City
-    {
-        public int Populated;
-        public string Name;
-        public int Area;
-        public double Density;
-    }
     class Program
     {
         static void Main(string[] args)
         {
             string[] result = Console.ReadLine().Split(';');
+            //string[] result = "Kharkiv=100000,350;kiev=2000000,200;Las Vegas=6000,100".Split(';');
+            string longestName = string.Empty;
+            string mostPopulated = string.Empty;
+            int maxPeopple = 0;
             City[] cities = new City[result.Length];
 
             for (int i = 0; i < cities.Length; i++)
             {
-                City newCity = new City();
-                string[] nameSplit = result[i].Split('=');
-                string[] values = nameSplit[1].Split(',');
-                newCity.Name = nameSplit[0];
-                newCity.Populated = int.Parse(values[0]);
-                newCity.Area = int.Parse(values[1]);
-                newCity.Density = Convert.ToDouble(newCity.Populated) / Convert.ToDouble(newCity.Area);
-                cities[i] = newCity;
-            }
-
-            string longestName = string.Empty;
-            string mostPopulated = string.Empty;
-            int maxPeopple = 0;
-
-            for (int i = 0; i < cities.Length; i++)
-            {
+                cities[i] = new City(result[i]);
                 if (longestName.Length < cities[i].Name.Length)
                 {
                     longestName = cities[i].Name;
